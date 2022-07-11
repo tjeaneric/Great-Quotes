3a12b885f1127f35dab92fa549db6f53b18e06c0
tjeaneric <ericjohn415@gmail.com>
2022-07-11 11:41:47 +0200

    added firebase hosting and lazy loading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
+import React, { Suspense } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 
 import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import QuoteDetails from "./pages/QuoteDetails";
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const QuoteDetails = React.lazy(() => import("./pages/QuoteDetails"));
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate to="/quotes" />} />
-        <Route path="quotes" element={<AllQuotes />} />
-        <Route path="quotes/:quoteId/*" element={<QuoteDetails />} />
-        <Route path="new-quote" element={<NewQuote />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate to="/quotes" />} />
+          <Route path="quotes" element={<AllQuotes />} />
+          <Route path="quotes/:quoteId/*" element={<QuoteDetails />} />
+          <Route path="new-quote" element={<NewQuote />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
